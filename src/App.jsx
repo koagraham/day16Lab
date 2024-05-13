eac38fee3c7b4b1ae1d7d148c4a42136d8a9b2f9
Koa Graham <koagraham233@gmail.com>
2024-05-13 10:36:18 -0600

    successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
+import playerData from './playerData.js';
+import { useState } from 'react'
+
+const cards = playerData.map((card) => <BaseballCard name={card.name} team={card.team} pos={card.position} stats={card.stats} url={card.imgUrl} key={card.cardId} />)
+
 function BaseballCard(props) {
-  return (
-    <div className="card">
-      <h2>Player Name goes here</h2>
-    </div>
-  );
+  let [showPicture, setPicture] = useState(true)
+  function toggleCard() {
+    if (showPicture) {
+      setPicture(showPicture = false)
+    }
+    else {
+      setPicture(showPicture = true)
+    }
+  }
+  if (showPicture) {
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <img src={props.url}/>
+      </div>
+    );
+  }
+  else {
+    const statsDisplay = []
+    for (const [key, value] of Object.entries(props.stats)) {
+      statsDisplay.push(<p>{key}: {value}</p>)
+    }
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <p>{props.team}</p>
+        <p>{props.pos}</p>
+        <p>{statsDisplay}</p>
+      </div>
+    )
+  }
 }
 
 function App() {
-  return <BaseballCard />;
+  return <>{cards}</>;
 }
 
 export default App;
